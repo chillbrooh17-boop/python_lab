47148f3f014ee459251eab3fcf24bb22f21433c4
chillbrooh17-boop <chillbrooh17@gmail.com>
2026-02-11 05:35:24 +0200

    Add files via upload

---
+++ io.py
@@ -0,0 +1,14 @@
+x=20
+print(x)
+y=3.43
+print(y)
+x="hello world"
+print(x)
+a=True
+print(a)
+name=input("enter your name:")
+print("hello",name)
+import sys
+print(sys.argv)
+number=int(input("enter number:"))
+print(number)
