70323de8c19c5591ab8eab720b8ad9e14eebf9fe
chillbrooh17-boop <chillbrooh17@gmail.com>
2026-02-23 11:21:18 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,24 @@
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+    num = 1
+
+while num <= 10:
+    if num % 2 == 0:
+        print(num)
+    num += 1
+    i = 1
+sum = 0
+
+while i <= 10:
+    sum += i
+    i += 1
+
+print("Sum is:", sum)
+num = int(input("Enter a number: "))
+i = 1
+while True:
+    print("This will run forever")
